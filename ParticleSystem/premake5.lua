5c060d7320eb2a888ce9dcc5704b99bf0e7285c5
tangxiaolong <xltangcs@outlook.com>
2024-01-03 21:30:40 +0800

    First commmit

---
+++ ParticleSystem/premake5.lua
@@ -0,0 +1,43 @@
+project "ParticleSystem"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++17"
+   staticruntime "off"
+
+   files 
+   { 
+      "src/**.h", 
+      "src/**.cpp",
+   }
+
+   includedirs
+   {
+      "../vendor/imgui",
+      "../vendor/glfw/include",
+   }
+
+
+   targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+   objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
+
+   filter "system:windows"
+      systemversion "latest"
+      defines { "PLATFORM_WINDOWS" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      runtime "Debug"
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "RELEASE" }
+      runtime "Release"
+      optimize "On"
+      symbols "On"
+
+   filter "configurations:Dist"
+      kind "WindowedApp"
+      defines { "DIST" }
+      runtime "Release"
+      optimize "On"
+      symbols "Off"
---
+++ premake5.lua
@@ -0,0 +1,9 @@
+-- premake5.lua
+workspace "ParticleSystem"
+   architecture "x64"
+   configurations { "Debug", "Release", "Dist" }
+   startproject "ParticleSystem"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "ParticleSystem"
