eb2bf282a41d1d37bd1655e5671a1e08fbd140d0
tangxiaolong <xltangcs@outlook.com>
2024-01-09 15:00:46 +0800

    Ray Tracing in one weekend
    Signed-off-by: tangxiaolong <xltangcs@outlook.com>

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -27,17 +27,9 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	{
 		m_FinalImage = std::make_shared<Toffee::Image>(width, height, Toffee::ImageFormat::RGBA);
 	}
+
 	delete[] m_ImageData;
 	m_ImageData = new uint32_t[width * height];
-	delete[] m_AccumulationData;
-	m_AccumulationData = new glm::vec4[width * height];
-
-	m_ImageHorizontalIter.resize(width);
-	m_ImageVerticalIter.resize(height);
-	for (uint32_t i = 0; i < width; i++)
-		m_ImageHorizontalIter[i] = i;
-	for (uint32_t i = 0; i < height; i++)
-		m_ImageVerticalIter[i] = i;
 }
 
 void Renderer::Render(const Scene& scene, const Camera& camera)
@@ -45,49 +37,18 @@ void Renderer::Render(const Scene& scene, const Camera& camera)
 	m_ActiveCamera = &camera;
 	m_ActiveScene = &scene;
 
-	if (m_FrameIndex == 1)
-		memset(m_AccumulationData, 0, m_FinalImage->GetWidth() * m_FinalImage->GetHeight() * sizeof(glm::vec4));
-
-#define MT 1
-#ifdef MT
-	std::for_each(std::execution::par, m_ImageVerticalIter.begin(), m_ImageVerticalIter.end(),
-		[this](uint32_t y)
-		{
-			std::for_each(std::execution::par, m_ImageHorizontalIter.begin(), m_ImageHorizontalIter.end(),
-			[this, y](uint32_t x)
-			{
-				glm::vec4 color = PerPixel(x, y);
-				m_AccumulationData[x + y * m_FinalImage->GetWidth()] += color;
-
-				glm::vec4 accumulatedColor = m_AccumulationData[x + y * m_FinalImage->GetWidth()];
-				accumulatedColor /= (float)m_FrameIndex;
-
-				accumulatedColor = glm::clamp(accumulatedColor, glm::vec4(0.0f), glm::vec4(1.0f));
-				m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(accumulatedColor);
-			});
-		});
-#else
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
+			//for(int sample)
 			glm::vec4 color = PerPixel(x, y);
-			m_AccumulationData[x + y * m_FinalImage->GetWidth()] += color;
 
-			glm::vec4 accumulatedColor = m_AccumulationData[x + y * m_FinalImage->GetWidth()];
-			accumulatedColor /= (float)m_FrameIndex;
-
-			accumulatedColor = glm::clamp(accumulatedColor, glm::vec4(0.0f), glm::vec4(1.0f));
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(accumulatedColor);
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
 	}
-#endif
 
 	m_FinalImage->SetData(m_ImageData);
-	if (Accumulate)
-		m_FrameIndex++;
-	else
-		m_FrameIndex = 1;
 }
 
 glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y)
@@ -96,35 +57,18 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y)
 	ray.Origin = m_ActiveCamera->GetPosition();
 	ray.Direction = m_ActiveCamera->GetRayDirections()[x + y * m_FinalImage->GetWidth()];
 
-	glm::vec3 light(0.0f);
-	glm::vec3 contribution(1.0f);
+	glm::vec3 redColor(1.0f,0.0f, 0.0f);
+	glm::vec3 skyColor(0.6f, 0.7f, 0.9f);
 
-	for (int i = 0; i < m_Bounces;i++)
+	Renderer::HitPayload payload = TraceRay(ray);
+	if (payload.HitDistance < -0.0f)
 	{
-		Renderer::HitPayload payload = TraceRay(ray);
-		if (payload.HitDistance < 0.0f)
-		{
-			glm::vec3 skyColor = glm::vec3(0.6f, 0.7f, 0.9f);
-			//light += skyColor * contribution;
-			break;
-		}
-
-		//float intensity = glm::max(glm::dot(glm::normalize(-m_LightDir), payload.WorldNormal), 0.0f);
-
-		const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
-		const Material& material = m_ActiveScene->Materials[sphere.MaterialIndex];
-
-		contribution *= material.Albedo;
-		light += material.GetEmission();
-
-
-		ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.001f;
-		//ray.Direction = glm::reflect(ray.Direction, 
-		//	payload.WorldNormal + material.Roughness * Toffee::Random::Vec3(-0.5f,0.5f));
-		ray.Direction = glm::normalize(payload.WorldNormal + Toffee::Random::InUnitSphere());
+		return glm::vec4(skyColor, 1.0f);
 	}
+	const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
+	const Material& material = m_ActiveScene->Materials[sphere.MaterialIndex];
 
-	return glm::vec4(light, 1.0f);
+	return glm::vec4(material.Albedo, 1.0f);
 }
 
 Renderer::HitPayload Renderer::TraceRay(const Ray& ray)
